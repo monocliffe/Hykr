20d3269853643e79be2f2349faeb4766fa401c81
monocliffe <45094007+monocliffe@users.noreply.github.com>
2019-11-28 06:04:44 +0000

    Added more stats + ScrollView

--- app/src/main/java/ie/ul/studentmail/ronan/journeyentry/GraphActivity.java
+++ app/src/main/java/ie/ul/studentmail/ronan/journeyentry/GraphActivity.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 public class GraphActivity extends AppCompatActivity {
     private JourneyDataBase jDB;
+    private int totalSteps, totalJourneys;
+    private double totalDist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,7 @@ public class GraphActivity extends AppCompatActivity {
         GraphView graphStep = findViewById(R.id.stepGraph);
         LineGraphSeries stepSeries = new LineGraphSeries<>(generateData(1));
         stepSeries.setTitle("Step Log");
-        stepSeries.setColor(R.color.colorB);
+        stepSeries.setColor(getColor(R.color.colorB));
         stepSeries.setDrawDataPoints(true);
         stepSeries.setDataPointsRadius(10);
         stepSeries.setThickness(8);
@@ -43,7 +45,7 @@ public class GraphActivity extends AppCompatActivity {
         GraphView graphDist = findViewById(R.id.distGraph);
         LineGraphSeries distSeries = new LineGraphSeries<>(generateData(2));
         stepSeries.setTitle("Distance Log");
-        distSeries.setColor(R.color.colorB);
+        distSeries.setColor(getColor(R.color.colorB));
         distSeries.setDrawDataPoints(true);
         distSeries.setDataPointsRadius(10);
         distSeries.setThickness(8);
@@ -51,6 +53,15 @@ public class GraphActivity extends AppCompatActivity {
 
         TextView highDist = findViewById(R.id.highDist);
         highDist.setText("Highest Distance: " + String.format("%.2f km", distSeries.getHighestValueY()));
+
+        TextView totalDistView = findViewById(R.id.totalDist);
+        totalDistView.setText(getString(R.string.totalDist) + " " + String.format("%.2f km",totalDist));
+
+        TextView totalStepView = findViewById(R.id.totalSteps);
+        totalStepView.setText(getString(R.string.totalSteps) + " " + totalSteps);
+
+        TextView totalJourneyView = findViewById(R.id.totalJourney);
+        totalJourneyView.setText(getString(R.string.totalJourney) + " " + totalJourneys);
     }
 
     private DataPoint[] generateData(int type) {
@@ -58,12 +69,14 @@ public class GraphActivity extends AppCompatActivity {
         List<Journey> journeyList = jDB.getJourneyDAO().getJourneys();
 
         int count = journeyList.size();
+        totalJourneys = count;
         DataPoint[] values = new DataPoint[count];
 
         if(type == 1) {
             for (int i = 0; i < journeyList.size(); i++) {
                 DataPoint v = new DataPoint(i, journeyList.get(i).getSteps());
                 values[i] = v;
+                totalSteps += journeyList.get(i).getSteps();
             }
         }
 
@@ -71,6 +84,7 @@ public class GraphActivity extends AppCompatActivity {
             for (int i = 0; i < journeyList.size(); i++) {
                 DataPoint v = new DataPoint(i, journeyList.get(i).getDistance());
                 values[i] = v;
+                totalDist += journeyList.get(i).getDistance();
             }
         }
         return values;
