b23ea4a5cf51ff25066c3ae5e8c0318caa80293f
monocliffe <45094007+monocliffe@users.noreply.github.com>
2019-11-28 04:19:03 +0000

    Added Step Graph
    added graph of steps with data taken from journey database

--- app/src/main/java/ie/ul/studentmail/ronan/journeyentry/GraphActivity.java
+++ app/src/main/java/ie/ul/studentmail/ronan/journeyentry/GraphActivity.java
@@ -4,13 +4,20 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
 import android.view.View;
 
+import java.util.List;
+
 public class GraphActivity extends AppCompatActivity {
+    private JourneyDataBase jDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,25 @@ public class GraphActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        GraphView graph = (GraphView) findViewById(R.id.stepGraph);
+        LineGraphSeries series1 = new LineGraphSeries<>(generateData(1));
+        graph.addSeries(series1);
+
     }
 
+    private DataPoint[] generateData(int type) {
+        jDB = Room.databaseBuilder(getApplicationContext(),JourneyDataBase.class,"JourneyDatabase").allowMainThreadQueries().build();
+        List<Journey> journeyList = jDB.getJourneyDAO().getJourneys();
+
+        int count = journeyList.size();
+        DataPoint[] values = new DataPoint[count];
+
+        if(type == 1) {
+            for (int i = 0; i < journeyList.size(); i++) {
+                DataPoint v = new DataPoint(i, journeyList.get(i).getSteps());
+                values[i] = v;
+            }
+        }
+        return values;
+    }
 }
